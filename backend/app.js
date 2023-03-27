acd9bef4b2a8f9f5f421de42c3e76231360c993c
Bottle7 <brandonjbouchard@gmail.com>
2023-03-27 14:50:28 -0700

    Add CORS config to API.

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,30 @@ const { healthRouter, productsRouter, notFoundRouter } = require('./routes/index
 
 const app = express();
 
+/**
+ * CORS Config
+ */
+app.use((_, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Don't do this in prod
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allowable HTTP Methods
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/**
+ * Base path
+ */
 app.get('/', (_, res) => res.send('Server is live!'));
+
+/**
+ * Routers
+ */
 app.use('/health', healthRouter);
 app.use('/products', productsRouter);
 
 // 404 page for any non-matching URLs
 app.use('*', notFoundRouter);
-
-
 
 module.exports = app;
