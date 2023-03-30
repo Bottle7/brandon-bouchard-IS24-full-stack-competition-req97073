2865c089426500981a5ea9da96530e377173122f
Bottle7 <brandonjbouchard@gmail.com>
2023-03-30 10:45:50 -0700

    Search clear on resubmit.

--- frontend/src/components/public/Search.jsx
+++ frontend/src/components/public/Search.jsx
@@ -15,6 +15,7 @@ const Search = () => {
 
   const onSubmit = async (formData) => {
     try {
+      setProducts([]);
       const { data } = await axios.get(`http://localhost:3000/api/products/${formData.endpoint}/${formData.searchTerm}`);
       setProducts(data);
       console.log(data)
